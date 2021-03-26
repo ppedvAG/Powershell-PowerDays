6eef8b650cf8c3fdd5fcf575d6e242a3e890414a
Stefan Ober <StefanO@ppedv.de>
2021-03-26 10:02:42 +0100

    Beispiel für die Verwendung von .Net Objekten in der PowerShell

---
+++ PowerShell-with-DotNet/OutVoice.ps1
@@ -0,0 +1,21 @@
+﻿
+#Add-Type ermöglicht das laden von DLL / oder .Net Objekten in der Powershell
+Add-Type -AssemblyName System.Speech
+
+#erzeugen einer neuen Speech Synthesizer Instanz
+$speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
+
+#Rate = Geschwindigkeit zwischen -10 und +10. Default Wert is 0
+$speak.Rate = -3
+
+#Abfragen der verfügbaren Stimmen
+$voices = $speak.GetInstalledVoices().VoiceInfo
+
+$speak.SelectVoice($voices[1].Name)
+
+
+#Ausführen der Speak Methode
+$speak.Speak("Would you like to play a game?")
+
+#Am Ende der Verwendung von .NET Objekten oder Assemblies sollten dieses Disposed werden
+$speak.Dispose() 
