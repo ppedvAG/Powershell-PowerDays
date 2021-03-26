2a585b109345f50019d21395beadac0e56a5b6e9
Stefan Ober <StefanO@ppedv.de>
2021-03-26 11:17:19 +0100

    Beispiel für die Verwendung von WinForms in Powershell

---
+++ PowerShell-with-DotNet/PowerShell with Winform.ps1
@@ -0,0 +1,15 @@
+﻿[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing")
+[void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")
+
+$windows = New-Object System.Windows.Forms.Form
+
+$windows.StartPosition = "CenterScreen"
+
+$textbox = New-Object System.Windows.Forms.TextBox
+$textbox.Location = New-Object System.Drawing.Size(300,100)
+$textbox.Size = New-Object System.Drawing.Size(200,20)
+
+$windows.Controls.Add($textbox)
+
+
+[void] $windows.ShowDialog()
