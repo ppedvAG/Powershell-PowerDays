7068dc7e520e9685f12e9742d456c4c97ac5ba90
Stefan Ober <StefanO@ppedv.de>
2021-03-26 15:02:57 +0100

    Beispiel für einen PowerShell Job hinzugefügt

---
+++ JobExample/psjobexample.ps1
@@ -0,0 +1,7 @@
+﻿$job1 = Start-Job -ScriptBlock {"Do Something"}
+$job2 = Start-Job -ScriptBlock {"Do Something different"}
+ 
+do
+{#Schleife zum warten bis beide Jobs fertig sind
+Start-Sleep -Milliseconds 50
+}until($job1.Status -eq "Completed" -and $job2.Status -eq "Completed")
